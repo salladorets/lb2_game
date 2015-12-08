e3bbe603c1735ebac765bf7edfa435e08c6ee3f4
Rainbow-Raccoon <Rainbow.racoon10@gmail.com>
2015-12-08 09:13:01 +0200

    main_menu+history

--- app/src/main/java/ua/example/alex/lb2_game/MainActivity.java
+++ app/src/main/java/ua/example/alex/lb2_game/MainActivity.java
@@ -1,19 +1,23 @@
 package ua.example.alex.lb2_game;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView txt;
-    Button btn1, btn2, btn3, btn4, btn5, btn6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    
+    public void onClick_Pre(View view) {
+        Intent clk=new Intent(this, Pre_History.class);
+        startActivity(clk);
+    }
+    public void onClick_Start(View view){
+
+    }
 }
