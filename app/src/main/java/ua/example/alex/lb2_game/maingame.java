663e33eb8b04bb537a1a43695d6ca79dbef2ad75
salladorets <salladorets@gmail.com>
2015-12-08 08:40:58 +0200

    fStart

--- app/src/main/java/ua/example/alex/lb2_game/maingame.java
+++ app/src/main/java/ua/example/alex/lb2_game/maingame.java
@@ -2,18 +2,12 @@ package ua.example.alex.lb2_game;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class maingame extends AppCompatActivity {
 
-    TextView txt;
-    Button btn1, btn2, btn3, btn4, btn5, btn6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_maingame);
     }
-
-    
 }
